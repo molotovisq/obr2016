707e583d4fcd4a73c4b4782f362c10d00ad2d714
José Marcolino <josephgdroid@gmail.com>
2016-05-31 16:24:10 -0300

    Rotina de teste de motores adicionada

---
+++ motorTester/motorTester.ino
@@ -0,0 +1,25 @@
+
+const int minRange = 4;
+const int maxRange = 13;
+const int pwm = 200;
+const int interval = 2000;
+int port = minRange - 1;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (port < maxRange) {
+    port++;
+    analogWrite(port, pwm);
+    Serial.println(port);
+    delay(interval);
+    analogWrite(port, 0);
+  }
+
+  if (port == maxRange)
+    port == minRange;
+
+}
