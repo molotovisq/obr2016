0ecb37715b6c7bfd403867425f18a9b9c38f8ec3
José Marcolino <josephgdroid@gmail.com>
2016-05-31 11:57:17 -0300

    Codigo de teste da matriz de sensores adicionado, opcao com nomes e sem nomes

---
+++ testeSensoresLinha/testeSensoresLinha.ino
@@ -0,0 +1,37 @@
+const int quantityOfSensors = 8;
+const int sensorPort[quantityOfSensors] = {A7, A6, A5, A4, A3, A2, A1, A0};
+bool withNames = 0;
+int sensorValue[quantityOfSensors];
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int i = 0; i < quantityOfSensors; i++) {
+    sensorValue[i] = analogRead(sensorPort[i]);
+  }
+
+  if (withNames == 1) {
+    for (int j = 0; j < quantityOfSensors; j++) {
+      Serial.print(" Sensor ");
+      Serial.print(j);
+      Serial.print(": ");
+      Serial.print(sensorValue[j]);
+
+      if (j == 7)
+        Serial.println("");
+    }
+    
+  } else {
+    
+    for (int j = 0; j < quantityOfSensors; j++) {
+      Serial.print(sensorValue[j]);
+      Serial.print(" ");
+
+      if (j == 7)
+        Serial.println("");
+    }
+  }
+}
